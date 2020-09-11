8483202330f3c31aa995cb22564a28f7bbd6b8da
veax <borissowadim@mail.ru>
2020-09-11 20:47:50 +0200

    part 1 course_info

--- exercises/src/part2/phonebook/components/SearchFilter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SearchFilter = ({ handleSearch }) => (
-  <div>
-    filter shown with <input type="text" onChange={handleSearch} />
-  </div>
-);
-
-export default SearchFilter;
